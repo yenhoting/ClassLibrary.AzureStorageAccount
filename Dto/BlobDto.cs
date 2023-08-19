4970aa6763dac0c63cc9a24e15e2fc17db38b316
Justin Yen <justinyen@systexsoftware.com.tw>
2023-08-19 16:28:23 +0800

    加入專案檔案。

---
+++ Dto/BlobDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassLibrary.AzureStorageAccount
+{
+    public class BlobDto
+    {
+        public string Uri { get; set; }
+        public string Name { get; set; }
+        public string CreateOn { get; set; }
+        public string ContentType { get; set; }
+        public Stream Content { get; set; }
+    }
+}
